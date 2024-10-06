2c34db92aaab9246da2b99c4913fdae7c796dd66
phmquan <phanhoangminhquan240402@gmail.com>
2024-10-06 16:36:21 +0700

    init backend commit

---
+++ backend/SE405_P11_RealEstateApp/src/test/java/com/example/SE405_P11_RealEstateApp/Se405P11RealEstateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SE405_P11_RealEstateApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se405P11RealEstateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
