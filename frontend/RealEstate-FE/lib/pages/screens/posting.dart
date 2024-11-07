35356ffc66501cdcbbc1cfa715597c923e2629b6
helloworld32144 <nguyenthilinhchi205@gmail.com>
2024-11-07 22:39:22 +0700

    FIX - Login, Sign Up

---
+++ frontend/RealEstate-FE/lib/pages/screens/posting.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Posting extends StatefulWidget {
+  const Posting({super.key});
+
+  @override
+  State<Posting> createState() => _PostingState();
+}
+
+class _PostingState extends State<Posting> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Đăng tin", style: TextStyle(fontSize: 40),),
+      ),
+    );
+  }
+}
